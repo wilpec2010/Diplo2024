c65f9757c79be601ae36dc7839e8b9c6c5d6a7f9
wilson perez <wilpec2017@gmail.com>
2024-01-16 18:52:20 -0500

    Mis primeras funciones v1

---
+++ funciones.py
@@ -0,0 +1,20 @@
+import os
+os.system("cls")
+
+# ****************************************************************
+
+def suma (a,b):
+    c = a + b
+    print("La suma de a + b es: ",c)
+
+# suma(2,3)
+
+def resta (a,b):
+    c = a - b
+    print("La resta de a - b es: ",c)
+# resta(2,3)
+      
+def multiplicacion (a,b):
+    pass
+def division (a,b):
+    pass
